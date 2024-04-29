8a5914cdc1359a74400657a55b42a8ffc58f6c7e
NareshDongala <163843430+NareshDongala@users.noreply.github.com>
2024-04-29 12:50:03 +0530

    BookingController.java

--- src/main/java/com/jsp/projectm2/controller/BookingController.java
+++ src/main/java/com/jsp/projectm2/controller/BookingController.java
@@ -1,5 +1,13 @@
 package com.jsp.projectm2.controller;
 
 public class BookingController {
-
+  @PostMapping
+	public ResponseEntity<ResponseStructure<Booking>> addBooking(@RequestParam int customerId,
+			@RequestParam int[] medicineId,@RequestBody BookingDto bookingDto){
+		return service.addBooking(customerId,medicineId,bookingDto);
+	}
+	@DeleteMapping
+	public ResponseEntity<ResponseStructure<Booking>>  cancelBooking(@RequestParam int bookingId){
+		return service.cancelBooking(bookingId);
+	}
 }
